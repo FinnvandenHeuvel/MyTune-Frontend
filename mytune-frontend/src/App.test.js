3eb40918826a0ffe38929fb1e0c03107e4807f16
I561104 <f.vandenheuvel@student.fontys.nl>
2026-01-09 14:07:04 +0100

    fixed unit tests failing and linting

--- mytune-frontend/src/App.test.js
+++ mytune-frontend/src/App.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import App from "./app/App";
+import { render, screen } from '@testing-library/react';
+import App from './app/App';
 
-test("renders main navigation", () => {
-    render(<App />);
+test('renders main navigation', () => {
+  render(<App />);
 
-    expect(screen.getByRole("button", { name: /all reviews/i })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /search artists/i })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /login/i })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /register/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /all reviews/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /search artists/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /register/i })).toBeInTheDocument();
 });
